468bbc2885d7bf92e2b59ee586832c5781755bbc
AndreKaba <andre.kaba@gmail.com>
2015-11-13 09:55:02 +0100

    powerConsumption remake

--- src/main/java/com/vires/sensorapi/objects/SensorPowerConsumption.java
+++ src/main/java/com/vires/sensorapi/objects/SensorPowerConsumption.java
@@ -3,10 +3,12 @@ package com.vires.sensorapi.objects;
 import com.vires.sensorapi.data.sensor_data.PowerConsumption;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * @author Filip Prochazka (jacktech24)
+ * @author Ondrej Kaba (Okaba)
  */
 public class SensorPowerConsumption {
 
@@ -15,16 +17,17 @@ public class SensorPowerConsumption {
     private Long dateTo;
 
     private SensorPowerConsumption(List<PowerConsumption> list) {
-        setDateFrom(Long.MAX_VALUE);
-        setDateFrom(Long.MIN_VALUE);
-        Float cons = (float) 0;
-        for(int i = 0; i<list.size(); i++){
-            if (list.get(i).getTimestamp()<dateFrom) setDateFrom(list.get(i).getTimestamp());
-            if (list.get(i).getTimestamp()>dateTo) setDateTo(list.get(i).getTimestamp());
-            cons += list.get(i).getConsumption();
+        Collections.sort(list);
+        setDateFrom(list.get(0).getTimestamp());
+        setDateTo(list.get(list.size()-1).getTimestamp());
+        Float consumption = (float) 0;
+        for(int i = 0; i<list.size()-1;i++){
+            Long timespan = list.get(i+1).getTimestamp()-list.get(i).getTimestamp();
+            consumption += (list.get(i).getConsumption()*timespan)/3600000;
         }
-        setTotalConsumption((cons/list.size())*(dateTo-dateFrom)*3600000); // recorded consumption / number of entries - avg consumption
-    }                                                                      // *seconds in timespan (J) -> *3600000 (KWh)
+        consumption += list.get(list.size()-1).getConsumption()/3600000;
+        setTotalConsumption(consumption);
+    }
 
     public static SensorPowerConsumption from(List<PowerConsumption> sensors) {
         return new SensorPowerConsumption(sensors);
